6494224b32249954e093d427693aa52a8b72e858
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-06-06 22:27:48 +0530

    Third Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'myapp';
+  title = "First-Angular-Assignment";
+  name = "Ashutosh Anand";
+  isDisabled = true;
+  boolean = true;
+  text = "Default Value";
+  items = [
+    "My Hero Academia",
+    "Shingeki no Kyogin",
+    "Black Clover",
+    "Seven Deadly Sins",
+  ];
+  flag = true;
+
+  change() {
+    this.isDisabled = !this.isDisabled;
+    this.boolean = !this.boolean;
+  }
+
+  changeFlag() {
+    this.flag = !this.flag;
+  }
 }
